c6f94a8dbfa8133a95988b51b9c7e87011037330
Matthew Thompson <mbthompson14@gmail.com>
2023-10-16 18:33:55 -0600

    super duper recursive function in progress

--- XNATSync.py
+++ XNATSync.py
@@ -64,7 +64,7 @@ class XNATSync:
             if project_id in session.projects:
                 project = session.projects[project_id]
 
-                self._test_download(session=session, parent_dir=project, local_root=local_root)
+                self._check_download(session=session, parent_dir=project, local_root=local_root)
 
                 # self._upload
 
@@ -73,21 +73,57 @@ class XNATSync:
             
 
 
+    # recursively check if directory/file exists
+    # if no, try to download
+    # if yes, check if child directories/files exists
+    # etc.
+    def _check_download(self, session, parent_dir, local_root: str) -> None:
+        
+        if isinstance(parent_dir, self._xnat):
+            child_attr = 'subjects'
+        elif isinstance(parent_dir, self._xnat.Subject):
+            child_attr = 'experiments'
+        elif isinstance(parent_dir, self._xnat.Experiment):
+            child_attr = 'scans'
+        elif isinstance(parent_dir, self._xnat.Scan):
+            child_attr = 'resources'
+        elif isinstance(parent_dir, self._xnat.Resource):
+            child_attr = 'files'
+        elif isinstance(parent_dir, self._xnat.File):
+            child_attr = None
+        else:
+            raise Exception("Object is wrong type")
 
-    def _test_download(self, session, parent_dir, local_root: str) -> None:
+        #for attr in ('subjects','experiments','scans','resources'):
+        if hasattr(parent_dir, child_attr):
+            for child_dir in getattr(parent_dir,child_attr).values():
+                uri = child_dir.uri
 
-        for attr in ('subjects','experiments','scans','resources'):
-            if hasattr(parent_dir, attr):
-                for child_dir in getattr(parent_dir,attr).values():
-                    uri = Path(child_dir.uri)
-                    path = Path(local_root)
-
-
-
-
+                if isinstance(child_dir, (self._xnat.Experiment, self._xnat.Scan, self._xnat.Resource)):
+                    path = Path(local_root, Path(child_dir.uri[1:]).parts[:-1], child_dir.label)
+                else:
+                    path = Path(local_root, child_dir.uri[1:])
 
 
-    def _check_download(self, session, project, local_root: str) -> None:
+                if path.is_dir():
+                    self._check_download(session=session, parent_dir=child_dir, local_root=local_root)
+                else:
+                    try:
+                        session.create_object(uri).download_dir(path.parts[:-3])
+                    except Exception as e:
+                        logging.warning(f'Problem downloading: {path}. Will try downloading as sub-directories/files.\nOriginal exception: {e}')
+                        self._check_download(session=session, parent_dir=child_dir, local_root=local_root)
+
+
+        else:
+            pass
+
+
+
+
+
+
+    def _check_download_old(self, session, project, local_root: str) -> None:
 
         for subject in project.subjects.values():
 
@@ -140,14 +176,14 @@ class XNATSync:
             else:
                 self._download(session=session,parent_dir=subject)
 
-    def _download(self, session, parent_dir) -> None:
+    # def _download(self, session, parent_dir) -> None:
 
-        try:
-            session.create_object(parent_dir.custom_variables['uri']).download_dir(parent_dir.custom_variables['path'].parts[:-3])
-        except:
-            print('problemo')
-            for child_dir in parent_dir.custom_variables['children']:
-                self._download(session=session,parent_dir=child_dir)
+    #     try:
+    #         session.create_object(parent_dir.custom_variables['uri']).download_dir(parent_dir.custom_variables['path'].parts[:-3])
+    #     except:
+    #         print('problemo')
+    #         for child_dir in parent_dir.custom_variables['children']:
+    #             self._download(session=session,parent_dir=child_dir)
 
 
         # try:
