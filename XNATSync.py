63807d3fe24efe4e330c863385cdd050023a607f
Matthew Thompson <mbthompson14@gmail.com>
2023-11-03 17:20:30 -0600

    tested with upload and download functionality, added docstrings

--- XNATSync.py
+++ XNATSync.py
@@ -1,50 +1,117 @@
+#!/Users/matthew/anaconda3/bin/python
+# edit shebang to python path on nyx
+
+"""
+XNATSync class definition
+
+Matthew Thompson - 2023-11-03
+mbthompson14@gmail.com
+
+https://xnat.readthedocs.io/en/latest/index.html
+
+
+XNAT credentials must be stored in ~/.netrc in the following format:
+
+    machine xnatccn.semel.ucla.edu
+    login USERNAME
+    password PASSWORD
+
+The .netrc file can be created by running the following:
+
+    echo "machine xnatccn.semel.ucla.edu
+    >     login USERNAME
+    >     password PASSWORD" > ~/.netrc
+    chmod 600 ~/.netrc'
+
+"""
 
 import os
 import xnat
-import requests
+import timeit
 import logging
 import pathlib
-import timeit
 from pathlib import Path
 from datetime import datetime
 
 class XNATSync:
+    """
+    Collection of functions that use the XNATpy library to synchronize project directories on XNAT 
+    with local project directories
+    """
     def __init__(self, logs: str, log_level: int = logging.INFO) -> None:
+        """
+        Initialize class XNATSync
+
+        :logs: string to identify location to store logs, e.g. 'logs'
+        :log_level: level at which to log, e.g. logging.DEBUG, logging.INFO
+        :returns: None
+        """
         self._xnat = xnat
         self.logger = logging.basicConfig(level=log_level, 
                             format='%(asctime)s %(levelname)s: %(message)s',
                             handlers=[
                                 logging.FileHandler(f'{logs}/sync_xnat_{datetime.now()}.log', mode='w'),
-                                #logging.StreamHandler(sys.stdout)
                             ])
     
-    def sync(self, host: str, project_id: str, local_root: str, granularity: str = 'scans') -> None:
+    def sync(self, host: str, project_id: str, local_root: str, granularity: str = '') -> None:
+        """
+        Synchronize local project directory with project directory hosted on XNAT server.
 
+        NOTE: to establish connection with XNAT server, XNAT login credentials must be stored in ~/.netrc
+        in the following format:
+
+            machine xnatccn.semel.ucla.edu
+            login USERNAME
+            password PASSWORD
+
+        The .netrc file can be created by running the following:
+
+            echo "machine xnatccn.semel.ucla.edu
+            >     login USERNAME
+            >     password PASSWORD" > ~/.netrc
+            chmod 600 ~/.netrc'
+
+        :host: uri of XNAT server, e.g. 'https://xnatccn.semel.ucla.edu/'
+        :project_id: string of project ID in XNAT
+        :local_root: local directory containing the project directory
+        :granularity: level at which to download directories, default is resources, can be set to 'files'
+        :returns: None
+        """
         logging.info('BEGIN SYNC')
 
         with self._xnat.connect(server=host, default_timeout = 600, loglevel=logging.root.level) as session:
             if project_id in session.projects:
                 project = session.projects[project_id]
                 
-                #self._download_loop(session=session, project=project, local_root=local_root, granularity=granularity)
-
-                # depth
-                # subject = 1
-                # exp = 2
-                # scans = 4
+                self._download_loop(session=session, project=project, local_root=local_root, granularity=granularity)
 
                 self._upload(session=session, project=project, local_root=local_root, depth=4)
 
             else:
                 raise Exception("Project ID not found in XNAT")
 
-    def _download_loop(self, session: xnat.session.XNATSession, project: xnat.mixin.ProjectData, local_root: str, granularity: str = 'scans') -> None:
+    def _download_loop(self, session: xnat.session.XNATSession, project: xnat.mixin.ProjectData, 
+                       local_root: str, granularity: str = '') -> None:
+        """
+        Traverses the project directory on the XNAT server
+
+        May be possible to do this recursively
+
+        :session: XNATpy connection object
+        :project: XNATpy project object
+        :local_root: local directory containing the project directory
+        :granularity: level at which to download directories, default is resources, can be set to 'files'
+        :returns: None
+        """
+
+        logging.info('BEGIN DOWNLOAD')
 
         project_uri = Path('/data','projects',project.id)
         project_path = Path(local_root,project.id)
         project_path_index = 1
 
-        downloaded = self._check_download(session=session,uri=project_uri,path=project_path,path_index=project_path_index)
+        downloaded = self._check_download(session=session,uri=project_uri,path=project_path,
+                                          path_index=project_path_index)
         if downloaded:
             return
 
@@ -53,7 +120,8 @@ class XNATSync:
             subject_path = Path(project_path,subject.label)
             subject_path_index = 2
 
-            downloaded = self._check_download(session=session,uri=subject_uri,path=subject_path,path_index=subject_path_index)
+            downloaded = self._check_download(session=session,uri=subject_uri,path=subject_path,
+                                              path_index=subject_path_index)
             if downloaded:
                 continue
 
@@ -62,7 +130,8 @@ class XNATSync:
                 exp_path = Path(subject_path,exp.label)
                 exp_path_index = 3
 
-                downloaded = self._check_download(session=session,uri=exp_uri,path=exp_path,path_index=exp_path_index)
+                downloaded = self._check_download(session=session,uri=exp_uri,path=exp_path,
+                                                  path_index=exp_path_index)
                 if downloaded:
                     continue
 
@@ -71,7 +140,8 @@ class XNATSync:
                     scan_path = Path(exp_path,'scans','-'.join((scan.id,scan._overwrites['type'].replace(' ','_'))))
                     scan_path_index = 3
 
-                    downloaded = self._check_download(session=session,uri=scan_uri,path=scan_path,path_index=scan_path_index)
+                    downloaded = self._check_download(session=session,uri=scan_uri,path=scan_path,
+                                                      path_index=scan_path_index)
                     if downloaded:
                         continue
 
@@ -80,7 +150,8 @@ class XNATSync:
                         resource_path = Path(scan_path,'resources',resource.label)
                         resource_path_index = 3
 
-                        self._check_download(session=session,uri=resource_uri,path=resource_path,path_index=resource_path_index)
+                        self._check_download(session=session,uri=resource_uri,path=resource_path,
+                                             path_index=resource_path_index)
 
                         if granularity == 'files':
                             for file in resource.files.values():
@@ -96,47 +167,72 @@ class XNATSync:
                         else:
                             continue
 
-    def _check_download(self, session: xnat.session.XNATSession, uri: pathlib.PosixPath, path: pathlib.PosixPath, path_index: int) -> bool:
+    def _check_download(self, session: xnat.session.XNATSession, uri: pathlib.PosixPath, 
+                        path: pathlib.PosixPath, path_index: int) -> bool:
+        """
+        Checks if an object on the XNAT server exists in the local project directory. 
+        If the directory exists, does nothing. If the directory does not exist, attempts to download.
+
+        :session: XNATpy connection object
+        :uri: pathlib.Path uri of the object on the XNAT server
+        :path: pathlib.Path path to the local directory of the object
+        :path_index: integer specifying local directory level
+        :returns: boolean indicating whether a directory was downloaded
+        """
         
+        start = timeit.default_timer()
+
         logging.debug('In _check_download')
         logging.debug(f'uri: {uri}')
         logging.debug(f'path: {path}')
 
         if not path.is_dir() or not any(path.iterdir()):
-            logging.debug('Path does not exist or is empty. Attempting download.')
+            logging.info(f'Path {path} does not exist or is empty. Attempting download.')
             try:
                 session.create_object(str(uri)).download_dir(str(Path(*path.parts[:path_index])))
             except Exception as e:
                 logging.warning(f'Problem downloading: {path}. Will try downloading as sub-directories/files.')
-                logging.debug(f'Original exception: {e}')
+                logging.info(f'Original exception: {e}')
                 return False
             else:
-                logging.debug(f'Downloaded successfully: {path}')
+                logging.info(f'Downloaded successfully: {path}')
+                logging.debug(f'DOWNLOAD ELAPSED TIME : {path} : {timeit.default_timer() - start}')
                 return True
         else:
-            logging.debug('Path is a local directory and not empty. Moving on.')
+            logging.info(f'Path {path} is a local directory and not empty. Moving on.')
             return False
         
-    def _check_download_file(self, session: xnat.session.XNATSession, uri: pathlib.PosixPath, path: pathlib.PosixPath) -> None:
+    def _check_download_file(self, session: xnat.session.XNATSession, uri: pathlib.PosixPath, 
+                             path: pathlib.PosixPath) -> None:
+        """
+        Checks if a file object on the XNAT server exists in the local project directory. 
+        If the file exists, does nothing. If the file does not exist, attempts to download.
+
+        :session: XNATpy connection object
+        :uri: pathlib.Path uri of the object on the XNAT server
+        :path: pathlib.Path path to the local directory of the object
+        :returns: None
+        """
 
         logging.debug('In _check_download_file')
-        logging.debug(f'uri: {uri}')
-        logging.debug(f'path: {path}')
+        logging.debug(f'download uri: {uri}')
+        logging.debug(f'download path: {path}')
 
         if not path.is_file():
-            logging.debug('Path is not a local file. Attempting download.')
+            logging.info('Path is not a local file. Attempting download.')
             try:
                 session.create_object(str(uri)).download(str(path))
             except Exception as e:
                 logging.warning(f'Problem downloading file: {path}')
-                logging.debug(f'Original exception: {e}')
+                logging.info(f'Original exception: {e}')
             else:
-                logging.debug(f'Downloaded successfully: {path}')
+                logging.info(f'Downloaded successfully: {path}')
         else:
-            logging.debug('Path is a local file. Moving on.')
+            logging.info('Path is a local file. Moving on.')
             return        
 
-    def _upload(self, session: xnat.session.XNATSession, project: xnat.mixin.ProjectData, depth: int, local_root: str = '') -> None:
+    def _upload(self, session: xnat.session.XNATSession, project: xnat.mixin.ProjectData, 
+                depth: int, local_root: str = '') -> None:
         """
         Traverses local project directory, uploading folders to XNAT if they do not exist in XNAT
 
@@ -146,10 +242,11 @@ class XNATSync:
         E.g. if depth is set to 2, it will check and upload any subjects or experiments that exist in 
         the local directory but not in XNAT.
 
-        :session: XNAT connection object
-        :project: XNAT project object
+        :session: XNATpy connection object
+        :project: XNATpy project object
         :depth: level at which to scan and upload directories
         :local_root: the local root folder in which the project folder is contained
+        :returns: None
         """
 
         logging.info('BEGIN UPLOAD')
@@ -161,20 +258,14 @@ class XNATSync:
 
         for root, dirs, files in os.walk(top=project_path, topdown=True):
 
-            # check prearchive for fun
-            # for s in session.prearchive.find(project=project.id):
-            #     logging.debug(f'Prearchive: {s}, {s.status}')
-
             cur_depth = root.count(os.path.sep)
             if cur_depth - base_depth >= depth:
-                #logging.info('TOO DEEP, MOVING ON')
                 continue
 
             for dir in dirs:
                 full_path = str(Path(root,dir))
                 parts = Path(full_path.replace(f'{local_root}/','')).parts
                 xnat_dir = str(Path(*parts))
-                project_id = parts[0]
                     
                 if len(parts) == 1:
                     continue
@@ -208,10 +299,10 @@ class XNATSync:
                     logging.info(f'Directory {xnat_dir} does not exist in XNAT, attempting to upload...')
                     logging.debug(f'Original exception: {e}')
                     try:
-                        session.services.import_dir(directory=full_path, overwrite='append', project=project_id, subject=parts[1])
+                        session.services.import_dir(directory=full_path, overwrite='append', project=parts[0], subject=parts[1])
                     except Exception as e:
-                        logging.info(f'Directory {xnat_dir} failed to upload, will try uploading as subdirectories')
-                        logging.debug(f'Original exception: {e}')
+                        logging.warning(f'Directory {xnat_dir} failed to upload, will try uploading as subdirectories')
+                        logging.info(f'Original exception: {e}')
                     else:
                         logging.info(f'Directory {xnat_dir} successfully uploaded to XNAT')
                         logging.debug(f'UPLOAD ELAPSED TIME : {xnat_dir} : {timeit.default_timer() - start}')
@@ -221,21 +312,3 @@ class XNATSync:
                             successful_upload.append((parts[1],parts[2]))
                 else:
                     logging.info(f'Directory {xnat_dir} exists in XNAT, moving on to next directory or subdirectory')
-
-
-    def _search_prearchive(self, session: xnat.session.XNATSession, project, subject: str, exp: str) -> bool:
-        """
-        Intended to seach prearchive for directories currently in the process of uploading, archiving to XNAT
-        NOT IN USE
-        """
-
-        status_list = ('ARCHIVE PENDING','ARCHIVING NOW','BUILD PENDING','BUILDING NOW','READY')
-
-        for status in status_list:
-            prearchive_search = session.prearchive.find(project=project,subject=subject,session=exp,status=status)
-            logging.debug(f'PREARCHIVE SEARCH: {prearchive_search}')
-            if prearchive_search:
-                return True
-            else:
-                continue
-        return False
--- driver.py
+++ driver.py
@@ -1,3 +1,26 @@
+#!/Users/matthew/anaconda3/bin/python
+# edit shebang to python path on nyx
+
+"""
+Example driver for the XNATSync class
+
+Matthew Thompson - 2023-11-03
+mbthompson14@gmail.com
+
+XNAT credentials must be stored in ~/.netrc in the following format:
+
+    machine xnatccn.semel.ucla.edu
+    login USERNAME
+    password PASSWORD
+
+The .netrc file can be created by running the following:
+
+    echo "machine xnatccn.semel.ucla.edu
+    >     login USERNAME
+    >     password PASSWORD" > ~/.netrc
+    chmod 600 ~/.netrc'
+"""
+
 from XNATSync import XNATSync
 import argparse
 import logging
@@ -5,9 +28,9 @@ import os
 
 def main():
     HOST = 'https://xnatccn.semel.ucla.edu/'
-    PROJECT_ID = 'sync_test'
-    LOCAL = 'test_data'
-    LOGS = 'logs'
+    PROJECT_ID = 'sync_test'  # 'RADCO'
+    LOCAL = 'test_data'  # path to nyx raw dicoms folder
+    LOGS = 'logs'  # path to nyx logs folder
 
     # make directories if they don't exist
     os.makedirs(LOGS+'/', exist_ok=True) # can use pathlib if also need to make parent directories
@@ -24,8 +47,10 @@ def main():
                         action='store_true',
                         default=False)
     
+    # add option to check and download at the file level
     parser.add_argument('-f','--files',
-                        help='Check and download at the level of individual files. This takes a long time. (Default is to check and download at the level of scans)',
+                        help='Check and download at the level of individual files. This can take a long time. \
+                            (Default is to check and download at the level of resources)',
                         action='store_true',
                         default=False)
 
@@ -42,9 +67,9 @@ def main():
     if args.files:
         granularity = 'files'
     else:
-        granularity = 'scans'
+        granularity = ''
 
-    sync = XNATSync(logs=LOGS,log_level=logging.DEBUG)  # change log_level
+    sync = XNATSync(logs=LOGS,log_level=level)
     sync.sync(host=HOST,project_id=PROJECT_ID,local_root=LOCAL,granularity=granularity)
 
 if __name__ == '__main__':
